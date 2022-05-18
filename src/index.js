a49351d4e56d4abcaa343e0e032162466540b30c
dubisdev <dubisdev@gmail.com>
2022-05-18 20:24:59 +0200

    🚀🚀

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const name = "Plugin Name";
 const keyword = "kw";
 
 const plugin = ({ term, display, actions, settings, config, hide }) => {
-	display({
-		title: "Plugin Title",
-		getPreview: () => <h3>Hello world!</h3>,
-	});
+  display({
+    title: "Plugin Title",
+    getPreview: () => <h3>Hello world!</h3>,
+  });
 };
 
 const settings = {
-	example: {
-		type: "string",
-		defaultValue: "Example setting",
-		description: "👌🚀",
-	},
+  example: {
+    type: "string",
+    defaultValue: "Example setting",
+    description: "👌🚀",
+  },
 };
 
 export { plugin as fn, name, keyword, settings };
